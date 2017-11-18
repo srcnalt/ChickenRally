f31089593ee4669b8c5348002936e75f035a3a1e
Shalva Avanashvili <shalva.avanashvili@proekspert.ee>
2017-11-18 20:30:35 +0200

    added chicken movement script

---
+++ Assets/ChickenRally/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+	public int id;
+	public bool rotating;
+
+	void Update ()
+	{
+		if(rotating)
+			transform.Rotate(Vector3.forward, Time.deltaTime * 100);
+	}
+}
+
