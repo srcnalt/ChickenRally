6cede90c2770c23251501cb57f57c399c373415f
Shalva Avanashvili <shalva.avanashvili@proekspert.ee>
2017-11-18 20:21:46 +0200

    added scripts folder and camera movement script

---
+++ Assets/ChickenRally/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Transform chicken;
+	public Vector3 offset;
+	public float camSpeed;
+
+	void FixedUpdate ()
+	{
+		transform.position = chicken.TransformPoint(offset);
+		transform.rotation = Quaternion.Lerp(transform.rotation, chicken.rotation, Time.deltaTime * camSpeed);
+	}
+}
