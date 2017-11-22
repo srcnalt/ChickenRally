c9e7281bca12453c3c233bc0f9562c77168a4247
Shalva Avanashvili <shalva.avanashvili@proekspert.ee>
2017-11-22 18:44:10 +0200

    added chicken collision, camera shaking and bonus gaining logic

--- Assets/ChickenRally/Scripts/ChickenMovement.cs
+++ Assets/ChickenRally/Scripts/ChickenMovement.cs
@@ -7,16 +7,26 @@ public class ChickenMovement : MonoBehaviour
 	public float movementSpeed;
 	public float rotationSpeed;
 
+	private bool shouldStop = false;
+
 	void FixedUpdate ()
 	{
 		mhNormal = Input.GetAxis("Mouse X");
 
-		transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+		if (!shouldStop) {
+			transform.Translate (Vector3.forward * movementSpeed * Time.deltaTime);
 
-        #if UNITY_EDITOR
-            transform.Rotate(Vector3.up,  mhNormal * rotationSpeed * Time.deltaTime);
-        #else
+			#if UNITY_EDITOR
+			transform.Rotate (Vector3.up, mhNormal * rotationSpeed * Time.deltaTime);
+			#else
             transform.Rotate(Vector3.up,  (-1 * (Input.acceleration.x)) * rotationSpeed * Time.deltaTime);
-        #endif
+			#endif
+		}
     }
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Sphere") {
+			shouldStop = true;	
+		}
+	}
 }
